bf479cf1e8897912d7541d2c79e87308e85b9fe8
John Hashem <jphyqr@gmail.com>
2021-03-30 16:24:15 -0600

    add image

--- src/pages/index.js
+++ src/pages/index.js
@@ -28,6 +28,7 @@ function Index() {
       <p>
        We are bringing central texas style barbacue to 2167 Hamilton Street.  This style of cooking involves slowly smoking cuts of meat (Brisket, Ribs, Chickens, Turkeys) in a specially designed smoker that produces the cleanest fire, a salivating aroma, and the most tender, juicy and flavourful meal you can imagine.
       </p>
+      <img src="https://source.unsplash.com/random/400x200" alt="" />
       <br />
       <h2>The date according to Node.js (TypeScript) is:</h2>
       {/* <p>{date ? date : "Loading date..."}</p> */}
