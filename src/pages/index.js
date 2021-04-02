76266e11518d0a45a58024d437ccfc2e50939892
John Hashem <jphyqr@gmail.com>
2021-04-01 21:52:57 -0600

    force update

--- src/pages/index.js
+++ src/pages/index.js
@@ -6,11 +6,11 @@ import { useStore } from "react-redux";
 import VerticalSpacer from "../layout/VerticalSpacer";
 
 import "../styles/index.css";
+//test
+const MenuLayout = loadable(() => import("../layout/MenuLayout"));
 
-const MenuLayout= loadable(()=>import("../layout/MenuLayout"));
-
-function Index({data}) {
-  console.log(data)
+function Index({ data }) {
+  console.log(data);
   const [date, setDate] = useState(null);
   useEffect(() => {
     async function getDate() {
@@ -21,94 +21,91 @@ function Index({data}) {
     getDate();
   }, []);
 
-  
-  const store = useStore()
-  const state = store.getState()
-  console.log({state})
-
+  const store = useStore();
+  const state = store.getState();
+  console.log({ state });
 
   return (
-   <MenuLayout>
-   
+    <MenuLayout>
       <h1>ribsket</h1>
 
-<h3>14th and Hamilton</h3>
-<VerticalSpacer height="20"/>
-      <div className="background-img"  alt="brisket in regina" />
+      <h3>14th and Hamilton</h3>
+      <VerticalSpacer height='20' />
+      <div className='background-img' alt='brisket in regina' />
 
-<div className="menu">
-<Link to="/menu">
-    <div className="menu-item">Menu</div></Link>
- <Link to="/about">
- <div className="menu-item">About</div>
-   </Link>
+      <div className='menu'>
+        <Link to='/menu'>
+          <div className='menu-item'>Menu</div>
+        </Link>
+        <Link to='/about'>
+          <div className='menu-item'>About</div>
+        </Link>
+      </div>
 
+      <VerticalSpacer height='20' />
+      <h2>Texas Style BBQ in downtown Regina</h2>
+      <VerticalSpacer height='10' />
+      <p>
+        Ribsket is ... Ribs + Brisket with a variety of elevated sides that mix
+        central Texas with Saskatchewan comfort.{" "}
+      </p>
+      <VerticalSpacer height='20' />
+      <h2>The Pit</h2>
+      <VerticalSpacer height='10' />
+      <p>
+        {" "}
+        Our 500 gallon offset smoker was repurposed from a local farm, and
+        fabricated in Saskatchewan. It is designed to have the most consistent
+        draw and produce the cleanest burn.
+      </p>
 
-</div>
+      <style jsx>{`
+        .menu {
+          width: 100%;
+          display: flex;
+          justify-content: space-evenly;
 
-
-<VerticalSpacer height="20"/>
-<h2>Texas Style BBQ in downtown Regina</h2>
-<VerticalSpacer height="10"/>
-<p>Ribsket is ... Ribs + Brisket with a variety of elevated sides that mix central Texas with Saskatchewan comfort. </p>
-<VerticalSpacer height="20"/>
-<h2>The Pit</h2>
-<VerticalSpacer height="10"/>
-<p> Our 500 gallon offset smoker was repurposed from a local farm, and fabricated in Saskatchewan. It is designed to have the most consistent draw and produce the cleanest burn.</p>
-
-
-     <style jsx>{`
-     
-     .menu{
-       width: 100%;
-       display:flex;
-       justify-content: space-evenly;
-    
-       padding: 10px;
-       background-color: black;
-     }
-     .menu-item{
-       padding: 5px 10px 5px 10px;
-       background-color: yellow;
-       
-       color: red;
-      
-     }
-
-     .menu-item:hover{
-      color: white;
-      background-color:orange;  
-      transition: .2s all ease;
-     }
-
-
-     @media only screen and (max-width: 400px) {
-      .background-img{
-        width: 100vw;
-        height: 200px;
-       background-image: url(plate.png);
-       background-size: cover;
-       background-repeat:no-repeat;
-       background-position: center center;
-      }}
-      @media only screen and (min-width: 400px) {
-        .background-img{
-          width: 100vw;
-          height: 400px;
-         background-image: url(plate.png);
-         background-size: cover;
-         background-repeat:no-repeat;
-         background-position: center center;
+          padding: 10px;
+          background-color: black;
         }
-  }
-     `}</style>
-      </MenuLayout>
+        .menu-item {
+          padding: 5px 10px 5px 10px;
+          background-color: yellow;
+
+          color: red;
+        }
+
+        .menu-item:hover {
+          color: white;
+          background-color: orange;
+          transition: 0.2s all ease;
+        }
+
+        @media only screen and (max-width: 400px) {
+          .background-img {
+            width: 100vw;
+            height: 200px;
+            background-image: url(plate.png);
+            background-size: cover;
+            background-repeat: no-repeat;
+            background-position: center center;
+          }
+        }
+        @media only screen and (min-width: 400px) {
+          .background-img {
+            width: 100vw;
+            height: 400px;
+            background-image: url(plate.png);
+            background-size: cover;
+            background-repeat: no-repeat;
+            background-position: center center;
+          }
+        }
+      `}</style>
+    </MenuLayout>
   );
 }
 
 export default Index;
 
-//add image 
-
-
-
+//add image
