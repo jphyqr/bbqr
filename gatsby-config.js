5971574d2f61c5927d473903e349183d8d79bdfe
John Hashem <jphyqr@gmail.com>
2021-04-01 21:17:35 -0600

    commit key

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,3 +1,7 @@
+require("dotenv").config({
+  path: `.env.${process.env.NODE_ENV}`,
+});
+
 module.exports = {
   siteMetadata: {
     title: "Severus Snape",
@@ -12,7 +16,21 @@ module.exports = {
     {
       resolve: "gatsby-source-firestore",
       options: {
-        credential: process.env.firebase,
+        credential: {
+          type: "service_account",
+          project_id: "bbqr-6984c",
+          private_key_id: "73dcbb76fc9c40c9a294e4e47624792d84bbc47f",
+          private_key: process.env.firebaseKey,
+          client_email:
+            "firebase-adminsdk-fhnco@bbqr-6984c.iam.gserviceaccount.com",
+          client_id: "111469830084531883448",
+          auth_uri: "https://accounts.google.com/o/oauth2/auth",
+          token_uri: "https://oauth2.googleapis.com/token",
+          auth_provider_x509_cert_url:
+            "https://www.googleapis.com/oauth2/v1/certs",
+          client_x509_cert_url:
+            "https://www.googleapis.com/robot/v1/metadata/x509/firebase-adminsdk-fhnco%40bbqr-6984c.iam.gserviceaccount.com",
+        },
         types: [
           {
             type: "Book",
